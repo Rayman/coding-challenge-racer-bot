71effe45b33f674945828379cbf502e1ccb985c1
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2024-09-25 16:46:04 +0200

    Implement corner slow down

--- road_runner.py
+++ road_runner.py
@@ -1,6 +1,6 @@
 import json
 from argparse import Namespace
-from math import sqrt
+from math import sqrt, fmod, pi, radians
 from socket import socket, AF_INET, SOCK_DGRAM
 from typing import Tuple
 
@@ -12,13 +12,21 @@ from ...linear_math import Transform
 DEBUG = False
 
 
+def normalize_angle(angle):
+    result = fmod(angle + pi, 2.0 * pi)
+    if result <= 0.0:
+        return result + pi
+    return result - pi
+
+
 class RoadRunner(Bot):
     def __init__(self, track):
         super().__init__(track)
-        self.config = Namespace(corner_velocity=125, deceleration=197)
+        self.config = Namespace(corner_velocity=313, deceleration=107,
+                                corner_slow_down=2.2)
         if DEBUG:
             self.sock = socket(AF_INET, SOCK_DGRAM)
-            self.server_address = ('127.0.0.1', 12345)
+            self.server_address = ('127.0.0.1', 12389)
 
     @property
     def name(self):
@@ -34,18 +42,26 @@ class RoadRunner(Bot):
 
     def compute_commands(self, next_waypoint: int, position: Transform, velocity: Vector2) -> Tuple:
         target = self.track.lines[next_waypoint]
-        # calculate the target in the frame of the robot
-        target = position.inverse() * target
-        # calculate the angle to the target
-        angle = target.as_polar()[1]
+        next_target = self.track.lines[(next_waypoint + 1) % len(self.track.lines)]
 
-        max_velocity = sqrt(self.config.corner_velocity ** 2 + 2 * self.config.deceleration * target.length())
+        # calculate the target in the frame of the robot
+        relative_target = position.inverse() * target
+        # calculate the angle to the target
+        angle = relative_target.as_polar()[1]
+        next_angle = (target - position.p).angle_to(next_target - target)
+
+        corner_angle = abs(normalize_angle(radians((target - position.p).angle_to(next_target - target))))
+        corner_velocity = self.config.corner_velocity * (1 - self.config.corner_slow_down * corner_angle / pi)
+
+        max_velocity = sqrt(corner_velocity ** 2 + 2 * self.config.deceleration * relative_target.length())
 
         if DEBUG:
             data = {
                 'angle': angle,
                 'velocity': velocity.length(),
-                'max_velocity': max_velocity
+                'max_velocity': max_velocity,
+                'corner_angle': corner_angle,
+                'corner_velocity': corner_velocity,
             }
             self.sock.sendto(json.dumps(data).encode('utf-8'), self.server_address)
 
