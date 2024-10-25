f56db16755114cfbec6b183016bdac7c504ee113
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2024-10-25 08:39:55 +0200

    Work on vector pursuit

--- vector_pursuit.py
+++ vector_pursuit.py
@@ -1,5 +1,7 @@
+import json
 from argparse import Namespace
-from math import sqrt, atan2, fmod, pi
+from math import sqrt, atan2, fmod, pi, radians
+from socket import socket, AF_INET, SOCK_DGRAM
 from typing import Tuple
 
 from pygame import Vector2, Color
@@ -7,6 +9,8 @@ from pygame import Vector2, Color
 from ...bot import Bot
 from ...linear_math import Transform
 from ...track import Track
+
+DEBUG = True
 
 
 def crange(start, end, modulo):
@@ -25,27 +29,15 @@ class VectorPursuit(Bot):
     def __init__(self, track):
         super().__init__(track)
         self.config = Namespace(
-            corner_slow_down=30,
-            deceleration=122.35751522686678,
-            k=2,
+            corner_slow_down=1,
+            deceleration=50,
+            k=6,
         )
         self.target_speeds = []
         self.calculate_target_speeds(track)
-
-    def calculate_target_speeds(self, track: Track):
-        self.target_speeds = []
-        for i in range(len(track.lines)):
-            p0 = self.track.lines[(i - 1) % len(self.track.lines)]
-            p1 = self.track.lines[i]
-            p2 = self.track.lines[(i + 1) % len(self.track.lines)]
-            a = (p2 - p1).length()
-            b = (p0 - p2).length()
-            c = (p0 - p1).length()
-            area = 0.5 * abs(p0.x * (p1.y - p2.y) + p1.x * (p2.y - p0.y) + p2.x * (p0.y - p1.y))
-            R = a * b * c / (4 * area)
-
-            target_speed = self.config.corner_slow_down * R
-            self.target_speeds.append(target_speed)
+        if DEBUG:
+            self.sock = socket(AF_INET, SOCK_DGRAM)
+            self.server_address = ('127.0.0.1', 12389)
 
     @property
     def name(self):
@@ -64,7 +56,11 @@ class VectorPursuit(Bot):
         # calculate the target in the frame of the robot
         target = position.inverse() * target
         # calculate the angle to the target
-        target_angle = 0
+
+        # angle of next segment
+        target_angle = normalize_angle(radians((self.track.lines[(next_waypoint + 1) % len(self.track.lines)] -
+                                                self.track.lines[next_waypoint]).as_polar()[1]) - position.M.angle)
+        target_angle = -target_angle
         distance = target.length()
 
         try:
@@ -79,20 +75,55 @@ class VectorPursuit(Bot):
             term_2 = pow(distance, 2) / (2 * target.y)
 
             turning_radius = abs(term_1 * term_2)
-            print(
-                f'phi_1={phi_1:.2f}, phi_2={phi_2:.2f}, phi={phi:.2f}, term_1={term_1:.2f}, term_2={term_2:.2f}, turning_radius={turning_radius:.2f}')
+            if DEBUG:
+                data = {
+                    'target_angle': target_angle,
+                    'phi_1': phi_1,
+                    'phi_2': phi_2,
+                    'phi': phi,
+                    'term_1': term_1,
+                    'term_2': term_2,
+                    'turning_radius': turning_radius,
+                    'curvature': 1 / turning_radius,
+                }
+                self.sock.sendto(json.dumps(data).encode('utf-8'), self.server_address)
         except ZeroDivisionError:
             turning_radius = float('inf')
 
         target_speed = 100  # self.calculate_target_speed(next_waypoint, position)
         angular_velocity = target_speed / turning_radius
 
+        if target.y < 0:
+            angular_velocity *= -1
+
         if target_speed < velocity.length():
             throttle = -1
         else:
             throttle = 1
 
+            if DEBUG:
+                data = {
+                    'throttle': throttle,
+                    'angular_velocity': angular_velocity,
+                }
+                self.sock.sendto(json.dumps(data).encode('utf-8'), self.server_address)
+
         return throttle, angular_velocity
+
+    def calculate_target_speeds(self, track: Track):
+        self.target_speeds = []
+        for i in range(len(track.lines)):
+            p0 = self.track.lines[(i - 1) % len(self.track.lines)]
+            p1 = self.track.lines[i]
+            p2 = self.track.lines[(i + 1) % len(self.track.lines)]
+            a = (p2 - p1).length()
+            b = (p0 - p2).length()
+            c = (p0 - p1).length()
+            area = 0.5 * abs(p0.x * (p1.y - p2.y) + p1.x * (p2.y - p0.y) + p2.x * (p0.y - p1.y))
+            R = a * b * c / (4 * area)
+
+            target_speed = self.config.corner_slow_down * R
+            self.target_speeds.append(target_speed)
 
     def calculate_target_speed(self, next_waypoint: int, position: Transform):
         min_speed = float('inf')
